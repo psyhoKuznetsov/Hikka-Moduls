45be427b4cb5edceebefccea1fbda2d95660b02c
psyhoKuznetsov <armooqoak@gmail.com>
2025-02-17 06:30:08 +0100

    Create SaveFav.py

---
+++ SaveFav.py
@@ -0,0 +1,20 @@
+__version__ = (1, 0, 0)
+# meta developer: @psyho_Kuznetsov
+
+from .. import loader, utils
+from telethon import types
+
+@loader.tds
+class SimpleFavMod(loader.Module):
+    """Сохраняет сообщения, видео, файлы и т.д в избранное"""    
+    strings = {"name": "SaveFav"}
+
+    @loader.command()
+    async def fav(self, message):
+        """Сохраняет в избранное. Использование: .fav"""
+        if not message.is_reply:
+            return await message.edit("<b>❌ Ответьте на сообщение.</b>", parse_mode="html")            
+        reply = await message.get_reply_message()
+
+        await self._client.forward_messages("me", reply)
+        await message.edit("<b>✅ Успешно отправлено в избранное.</b>", parse_mode="html")
