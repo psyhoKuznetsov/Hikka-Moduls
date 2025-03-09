7a548e65bedcbcd488c5da1e93d26488d6264327
psyhoKuznetsov <armooqoak@gmail.com>
2025-03-09 15:29:14 +0100

    Update Purge.py

--- Purge.py
+++ Purge.py
@@ -1,4 +1,4 @@
-__version__ = (1, 0, 1)
+__version__ = (1, 0, 2)
 
 import asyncio
 from hikkatl.types import Message
@@ -6,25 +6,32 @@ from .. import loader, utils
 
 @loader.tds
 class Purge(loader.Module):
+    """Удаляет все сообщения в чате очень быстро"""
     strings = {"name": "Purge"}
 
     async def client_ready(self, client, db):
         self.client = client
-        self.db = db
 
     @loader.unrestricted
     async def purgecmd(self, message: Message):
+        """Полностью очистить чат"""
         chat = await message.get_chat()
         count = 0
-        
-        async for msg in self.client.iter_messages(chat):
-            try:
-                await msg.delete()
-                count += 1
+        msg_ids = []
+
+        async for msg in self.client.iter_messages(chat, reverse=True):
+            msg_ids.append(msg.id)
+            if len(msg_ids) >= 100: 
+                await self.client.delete_messages(chat, msg_ids)
+                count += len(msg_ids)
+                msg_ids = []
+
                 if count % 500 == 0:
                     await message.edit(f"🚮 Удалено: {count}")
-            except Exception:
-                pass
+
+        if msg_ids:
+            await self.client.delete_messages(chat, msg_ids)
+            count += len(msg_ids)
 
         await message.delete()
         await self.client.send_message(chat, f"✅ Успешно удалено {count} сообщений")
